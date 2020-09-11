8c92b4da06c2d1b73d569680e97c0711af919e28
TechFallMacBook <techfallmacbook@MacBook-Pro.home>
2020-09-11 11:08:56 -0400

    finished

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-var friends = ['Alex','WayneCarl','Jason','Jermaine','Branden']
+
+var friends = ['Alex','WayneCarl','Jason','peter','bob']
 var locations = ['study','wrestling room','laundry room', 'bedroom','courtyard','sex dungeon', 'attic','torture chamber','sauna','windjammer cafe']
 var weapons = ['hot dog','monkey wrench','knife','broad sword','spear gun','atomic bomb','acid','blow dart','bamboo under the finger nails','throwing shoe','chinese stars','loud rap music','large limp male genitalia','poison','drug overdose','strangulation','heckling','whining','a cold stare','limp bizkit']
 
-for (i=1; i<=100; i++){
-    var randomFriend
-    var randomLocation
-    var randomWeapon
-    function setStuff(){
-        randomFriend = friends[Math.floor(Math.random() * friends.length)]
-        randomLocation = locations[Math.floor(Math.random() * friends.length)]
-        randomWeapon = weapons[Math.floor(Math.random() * friends.length)]
-    }
-    setStuff()
-    var h3 = document.createElement('h3')
-    var h3Text = document.createTextNode(`Accusation ${i}`)
-    h3.appendChild(h3Text)
-    document.body.appendChild(h3)
-    h3.addEventListener('click',()=>{
-        alert(`I accuse ${randomFriend}, with the ${randomWeapon}, in the ${randomLocation}!`)
-    })
+function makeFunc(i){
+    return function() {
+        var randomFriend = friends[Math.floor(Math.random() * friends.length)]
+        var randomLocation = locations[Math.floor(Math.random() * locations.length)]
+        var randomWeapon = weapons[Math.floor(Math.random() * weapons.length)]
+        var h3 = document.createElement('h3')
+        var h3Text = document.createTextNode(`Accusation ${i}`)
+        h3.appendChild(h3Text)
+        document.body.appendChild(h3)
+        h3.addEventListener('click',()=>{
+            alert(`I accuse ${randomFriend}, with the ${randomWeapon}, in the ${randomLocation}!`)
+        })
+    }()
+}
+
+for (var i=1; i<=100; i++){
+    makeFunc(i)
 }
